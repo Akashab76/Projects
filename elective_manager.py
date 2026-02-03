6eeba6d530855ce256efbc251b17a81cc8dba646
Akashab76 <155179734+Akashab76@users.noreply.github.com>
2026-02-03 18:58:44 +0530

    Delete elective_manager.py

--- elective_manager.py
+++
@@ -1,7 +0,0 @@
-# elective_manager.py
-class ElectiveManager:
-    def __init__(self):
-        self.occupied = {}
-    def is_occupied(self, day, start): return self.occupied.get((day, start), False)
-    def assign(self, sem, sec, sub, day, start): self.occupied[(day, start)] = True
-    def clear(self): self.occupied.clear()
